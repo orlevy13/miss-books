847da6c39d46b38435b3e685220aaed6610f314e
orlevy13 <orlevy13d@gmail.com>
2020-06-22 19:09:47 +0300

    deploy

---
+++ precache-manifest.e68ec273e9761c79b2a4f2b4725b07d9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "627addb7e93eeda5a564",
+    "url": "/miss-books/css/app.be095273.css"
+  },
+  {
+    "revision": "dc7bceaa5cca20023e332a65fc9aa2a1",
+    "url": "/miss-books/fonts/berkshireswash-regular.dc7bceaa.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/miss-books/fonts/open-sans.3ed9575d.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/miss-books/fonts/roboto-black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/miss-books/fonts/roboto-bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/miss-books/fonts/roboto-light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/miss-books/fonts/roboto-regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/miss-books/fonts/roboto-thin.321de678.ttf"
+  },
+  {
+    "revision": "65099f98c7cb19b3dac57b15a6a708cf",
+    "url": "/miss-books/fonts/segoe-ui-bold.65099f98.ttf"
+  },
+  {
+    "revision": "0e7e9a9b5c4abaadef7bc8f4e4574084",
+    "url": "/miss-books/fonts/segoe-ui.0e7e9a9b.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/miss-books/fonts/ubuntu.2505bfbd.ttf"
+  },
+  {
+    "revision": "9001eb9c9c1519aaa7fcf26514c1dcfd",
+    "url": "/miss-books/img/bg.9001eb9c.png"
+  },
+  {
+    "revision": "093213241d29719ab4938bf45a7fd3de",
+    "url": "/miss-books/img/check.09321324.svg"
+  },
+  {
+    "revision": "12333ffe8a17ede83c2d63ec75cc1a45",
+    "url": "/miss-books/img/delete.12333ffe.svg"
+  },
+  {
+    "revision": "f19839a6fa030e3f814388d5c03ffa31",
+    "url": "/miss-books/img/sale.f19839a6.svg"
+  },
+  {
+    "revision": "34c1d993aa209907c107bd0d91b27999",
+    "url": "/miss-books/img/star.34c1d993.svg"
+  },
+  {
+    "revision": "a912415e681a9befcaf2151c87027a43",
+    "url": "/miss-books/index.html"
+  },
+  {
+    "revision": "6a40cebab691215c9d63",
+    "url": "/miss-books/js/about.103b8e6b.js"
+  },
+  {
+    "revision": "627addb7e93eeda5a564",
+    "url": "/miss-books/js/app.70129437.js"
+  },
+  {
+    "revision": "8b6b3a0f3e98ee6b6aef",
+    "url": "/miss-books/js/chunk-vendors.aaf98afe.js"
+  },
+  {
+    "revision": "f2660540af2f9d0e8bd9d3aa279d39a8",
+    "url": "/miss-books/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/miss-books/robots.txt"
+  }
+]);
